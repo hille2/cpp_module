78b00a240d4d1d7eeb3aa1ba309a7970ff6d01db
hille2 <hille.2@ya.ru>
2021-06-06 13:53:01 +0300

    ex03

--- module04/ex02/Squad.cpp
+++ module04/ex02/Squad.cpp
@@ -6,28 +6,28 @@
 /*   By: sgath <sgath@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 17:23:08 by sgath             #+#    #+#             */
-/*   Updated: 2021/06/06 11:51:23 by sgath            ###   ########.fr       */
+/*   Updated: 2021/06/06 13:32:34 by sgath            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Squad.hpp"
 
-Squad::Squad( ) :					countUnits(0), units(0)
+Squad::Squad( ) :					m_countUnits(0), units(0)
 {}
 
-Squad::Squad( Squad const &copy ) :	countUnits(copy.countUnits)
+Squad::Squad( Squad const &copy ) :	m_countUnits(copy.m_countUnits)
 {
-	if (countUnits > 0)
-		countUnits = 0;
-	for (int i = 0; i < copy.countUnits; i++)
+	if (m_countUnits > 0)
+		m_countUnits = 0;
+	for (int i = 0; i < copy.m_countUnits; i++)
 		units[i] = copy.units[i];
 }
 
 Squad::~Squad( )
 {
-	if (countUnits > 0)
+	if (m_countUnits > 0)
 	{
-		for (int i = 0; i < countUnits; i++)
+		for (int i = 0; i < m_countUnits; i++)
 			delete units[i];
 		delete units;
 	}
@@ -38,20 +38,20 @@ Squad			&Squad::operator=( Squad const &value )
 	if (&value == this)
 		return (*this);
 
-	if (countUnits > 0)
-		for (int i = 0; i < countUnits; i++)
+	if (m_countUnits > 0)
+		for (int i = 0; i < m_countUnits; i++)
 			units[i] = value.units[i];
 	return (*this);
 }
 
 int				Squad::getCount( ) const
 {
-	return (countUnits);
+	return (m_countUnits);
 }
 
 ISpaceMarine	*Squad::getUnit( int val ) const
 {
-	if ( val < 0 || val > countUnits)
+	if ( val < 0 || val > m_countUnits)
 		return (0);
 	return (units[val]);
 }
@@ -66,19 +66,19 @@ int				Squad::push( ISpaceMarine *soldier )
 			units = new ISpaceMarine*[0];
 		else
 		{
-			for (int i = 0; i < countUnits; i++)
+			for (int i = 0; i < m_countUnits; i++)
 				if (units[i] == soldier)
-					return (countUnits);
+					return (m_countUnits);
 			
-			ISpaceMarine **tmp = new ISpaceMarine*[countUnits + 1];
-			for (int i = 0; i < countUnits; i++)
+			ISpaceMarine **tmp = new ISpaceMarine*[m_countUnits + 1];
+			for (int i = 0; i < m_countUnits; i++)
 				tmp[i] = units[i];
 			delete [] units;
-			tmp[countUnits] = soldier;
+			tmp[m_countUnits] = soldier;
 			units = tmp;
 		}
-		units[countUnits] = soldier;
-		countUnits ++;
+		units[m_countUnits] = soldier;
+		m_countUnits ++;
 	}
-	return (countUnits);
+	return (m_countUnits);
 }
--- module04/ex02/Squad.hpp
+++ module04/ex02/Squad.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Squad.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yu <yu@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: sgath <sgath@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 17:15:19 by sgath             #+#    #+#             */
-/*   Updated: 2021/06/05 22:16:26 by yu               ###   ########.fr       */
+/*   Updated: 2021/06/06 13:32:34 by sgath            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Squad : public ISquad
 {
 private:
-	int				countUnits;
+	int				m_countUnits;
 	ISpaceMarine	**units;
 	
 public:
