f6f8a0d5fcbb3b2e411b8d88d2c8b7619278f821
hille2 <hille.2@ya.ru>
2021-06-19 17:16:17 +0300

    dont work with int :(

---
+++ module06/ex01/Serialization.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serialization.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yu <yu@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 13:40:22 by sgath             #+#    #+#             */
+/*   Updated: 2021/06/19 17:12:15 by yu               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZATION_HPP
+#define SERIALIZATION_HPP
+
+#include <iostream>
+#include <string>
+
+#define LEN 10
+
+struct Data
+{
+	std::string	rand_pointer1;
+	std::string	rand_pointer2;
+	int			num;
+};
+
+
+void			*serialize(void);
+Data			*deserialize(void * raw);
+
+#endif
+
+
+
+
---
+++ module06/ex01/main.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yu <yu@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 13:40:22 by sgath             #+#    #+#             */
+/*   Updated: 2021/06/19 12:19:10 by yu               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serialization.hpp"
+
+int randSymbolASCII()
+{
+	int num = rand() % 127;
+	if (isalpha(num) || isdigit(num))
+		return num;
+	return randSymbolASCII();
+}
+
+void	*serialize(void)
+{
+	char *data = new char[ LEN + LEN + 4];
+
+	std::cout << "Serialize:" << std::endl;
+	for (int i = 0; i < LEN; i ++)
+		data[i] = static_cast<char>(randSymbolASCII());
+	std::cout << "pointer to a random string:\t\t\t " << data << std::endl;
+	std::cout << "second pointer to a second random string:\t ";
+	for (int i = 0; i < LEN; i ++)
+	{
+		data[i + LEN ]= randSymbolASCII();
+	 	std::cout << data[i + LEN];
+	}
+	int num = rand() % INT32_MAX;
+	*(reinterpret_cast<int*>(data + LEN  + LEN) )= num;
+	std::cout << "\nrandom integer:\t\t\t\t\t " << num << std::endl;
+	
+	std:: cout << "---------------" << std::endl;
+	return reinterpret_cast<void*>(data);
+}
+
+Data	*deserialize(void *raw)
+{
+	Data *data = new Data;
+
+	std::cout << "Deserialize:" << std::endl;
+	data->rand_pointer1 = std::string(reinterpret_cast<char*>(raw), LEN);
+	std::cout << "pointer to a random string:\t\t\t " << data->rand_pointer1 << std::endl;
+	data->rand_pointer2 = std::string(reinterpret_cast<char*>(raw) + LEN, LEN);
+	std::cout << "second pointer to a second random string:\t " << data->rand_pointer2 << std::endl;
+	data->num = *(reinterpret_cast<int*>(raw) + LEN + LEN);
+	std::cout << "random integer:\t\t\t\t\t " << data->num << std::endl;
+
+	std:: cout << "---------------" << std::endl;
+	return data;
+}
+
+
+int		main()
+{
+	Data *data;
+
+	srand(static_cast<unsigned int>(time(0)));
+	void *raw = serialize();
+	data = deserialize(raw);
+
+	
+
+	return 0;
+}
