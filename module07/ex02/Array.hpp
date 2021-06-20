be86a557b98a63e85441a47f5270e3de13c35a42
hille2 <hille.2@ya.ru>
2021-06-20 17:21:08 +0300

    add ex 02

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,17 +6,17 @@
 /*   By: yu <yu@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 11:15:19 by sgath             #+#    #+#             */
-/*   Updated: 2021/06/20 17:10:44 by yu               ###   ########.fr       */
+/*   Updated: 2021/06/20 17:20:54 by yu               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_H
-#define ARRAY_H
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
 
 #include <iostream>
 #include <string>
 
-template<class T>
+template<typename T>
 class Array
 {
 private:
